3b275ef7e8b0fe88b760f6e396c1977c4379522a
ZitaTaka <51285627+ZitaTaka@users.noreply.github.com>
2022-01-08 00:51:18 +0900

    Temp Commit

--- src/lab/fxchart/index.php
+++ src/lab/fxchart/index.php
@@ -19,6 +19,30 @@
   <title><?php echo $symbol['display']; ?></title>
   <style type="text/css">
     body { margin: 0; padding: 0; }
+
+    /* portrait style */
+    @media screen and (orientation:portrait){
+      p {font-size: 1em; }  
+    }
+    /* landscape style */
+    @media screen and (orientation:landscape){
+      .block-chart {
+        float: left;
+        width: 70%;   height: 95%;
+      }
+      .block-support {
+        float: left;
+        width: 25%;   height: 95%;
+      }
+      .block-main {
+        float: left;
+        width: 100%;  height: 70%;
+      }
+      .block-sub {
+        float: left;
+        width: 50%;   height: 30%;
+      }
+    }
   </style>
   <script language="text/javascript">
       function build_screen(){
@@ -32,23 +56,51 @@
   </script>
 </head>
 <body>
-  <div id="block-hover" style="float: left; background-color:gray;" ></div>
-  <div id="block-header" style="float: left; background-color:Tomato;" ></div>
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   広告表示領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
   <div id="block-service" style="float: left; background-color:Tomato;" ></div>
-  <div id="block-chart-main" style="float: left; background-color:black;" >
-  main
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   ヘッダー領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
+  <div id="block-header" >
   </div>
-  <div id="block-chart-sub" style="float: left; background-color:black;" >
-  sub
-    <div id="block-chart-sub-1" style="float: left; background-color:blue;" >
-    sub1
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   チャート表示領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
+  <div id="block-chart" >
+    <div id="block-main" >
+    main
     </div>
-    <div id="block-chart-sub-2" style="float: left; background-color:blue;" >
-    sub2
+    <div id="block-sub"  >
+      <div id="block-sub-1" >
+      sub1
+      </div>
+      <div id="block-sub-2" >
+      sub2
+      </div>
     </div>
   </div>
-  <div id="block-footer" style="background-color:Tomato;" ></div>
-  <div id="block-debug" style="background-color:Tomato;" ></div>
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   サポート情報表示領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
+  <div id="block-support" >
+  </div>
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   フッター領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
+  <div id="block-footer" style="background-color:Tomato;" >
+  </div>
+
+<?php # ---- ---- ---- ---- ---- ---- ---- ----
+      #   デバッグ領域
+      # ---- ---- ---- ---- ---- ---- ---- ---- ?>
+  <div id="block-debug">
     <?php var_dump($_GET); ?>
     <?php var_dump($symbol); ?>
   </div>
